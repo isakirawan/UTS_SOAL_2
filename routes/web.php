e46a46342ece6978df148b2da6d0840fc0561d48
isakirawan <isakirawan119@gmail.com>
2022-10-27 14:24:09 +0700

    Jawaban Soal Nomor 2

--- routes/web.php
+++ routes/web.php
@@ -13,9 +13,9 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
 
 // Route::group(['prefix'=>'mahasiswa'], function(){
 //     Route::get('/pendaftaran', function () {
@@ -30,3 +30,6 @@ Route::get('/', function () {
 // });
 // Route::view('/mahasiswa', 'mahasiswa/index');
 
+Route::get('/mahasiswa', function() {
+    return View::make('mahasiswa.index');
+});
